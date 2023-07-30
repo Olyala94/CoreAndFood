e0e5be449f0b009c12ebb2e38d9104f6f813efec
Olya <119108499+Olyala94@users.noreply.github.com>
2023-07-30 07:35:18 +0300

    Add project files.

---
+++ CoreAndFood/Data/Models/Category.cs
@@ -0,0 +1,14 @@
+﻿namespace CoreAndFood.Data.Models
+{
+    public class Category
+    {
+        public int CategoryId { get; set; }
+
+        public string CategoryName { get; set; }
+
+        public string Description { get; set; }
+
+        //Bir Kategori birden fazla Food içerisine yer Alabiliyor!!!
+        public List<Food> Foods { get; set; }  //Ne tarafta Liste varsa demek ki burası işin -> Ana Kısmı olacak!!!!
+    }
+}
---
+++ CoreAndFood/Data/Models/Food.cs
@@ -0,0 +1,27 @@
+﻿namespace CoreAndFood.Data.Models
+{
+    public class Food
+    {
+        public int FoodId { get; set; }
+
+        public string FoodName { get; set; }
+
+        public string ShortDescription { get; set; }
+
+        public string LongDescription { get; set; }
+
+        public double Price { get; set; }
+
+        public string ImageURL { get; set; }
+
+        public string ThumbNailImageURL { get; set; }
+
+        public int Stock { get; set; }
+
+        //Burada ise sadece bir food'ın sadece bir tane Category'sinin olacagını bildirdik
+        public int CategoryId { get; set; }
+
+        //Category sınıfından türetmiş olduk 
+        public virtual Category Category { get; set; }
+    }
+}
