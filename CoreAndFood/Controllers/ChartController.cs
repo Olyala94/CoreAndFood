4446fc960ca80ba5d368e0fff1eae9eed436abe1
Olya <119108499+Olyala94@users.noreply.github.com>
2023-08-01 20:27:00 +0300

    # - Google Chart Kullanımı (PieChart & ColumnChart)

---
+++ CoreAndFood/Controllers/ChartController.cs
@@ -0,0 +1,45 @@
+﻿using CoreAndFood.Data.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreAndFood.Controllers
+{
+    public class ChartController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Index2()
+        {
+            return View();
+        }
+
+        public IActionResult VisualizeProductResult()
+        {
+            return Json(ProductList());
+        }
+        
+        public List<Class1> ProductList()
+        {
+            List<Class1> class1s = new List<Class1>();
+            class1s.Add(new Class1()
+            {
+                ProductName = "Computer",
+                Stock = 150
+            });
+            class1s.Add(new Class1()
+            {
+                ProductName="Lcd",
+                Stock = 75
+            });
+            class1s.Add(new Class1()
+            {
+                ProductName= "Usb Disk",
+                Stock=220
+            });
+
+            return class1s;
+        }
+    }
+}
---
+++ CoreAndFood/Data/Models/Class1.cs
@@ -0,0 +1,10 @@
+﻿namespace CoreAndFood.Data.Models
+{
+    public class Class1
+    {
+        public string ProductName { get; set; }
+
+        public int Stock { get; set; }
+    }
+
+}
