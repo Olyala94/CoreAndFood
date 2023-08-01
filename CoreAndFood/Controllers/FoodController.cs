93cc67213638f53515f2a041b6347d961ec94d3b
Olya <119108499+Olyala94@users.noreply.github.com>
2023-08-01 19:48:37 +0300

    # - Update Food # - PagedList

--- CoreAndFood/Controllers/FoodController.cs
+++ CoreAndFood/Controllers/FoodController.cs
@@ -2,6 +2,7 @@
 using CoreAndFood.Repository;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using X.PagedList;
 
 namespace CoreAndFood.Controllers
 {
@@ -10,9 +11,9 @@ namespace CoreAndFood.Controllers
         Context c = new Context(); 
         FoodRepository foodRepository = new FoodRepository(); 
 
-        public IActionResult Index()
+        public IActionResult Index(int page=1)
         {  
-            return View(foodRepository.TList("Category"));
+            return View(foodRepository.TList("Category").ToPagedList(page, 3));
         }
 
         [HttpGet]
@@ -40,6 +41,46 @@ namespace CoreAndFood.Controllers
           
             foodRepository.TDelete(new Food { FoodId = id });
             return RedirectToAction("Index");   
+        }
+
+        public IActionResult FoodGet(int id)
+        {
+            var values = foodRepository.TGetList(id);
+
+            List<SelectListItem> value = (from y in c.Categories.ToList()
+                                           select new SelectListItem
+                                           {
+                                               Text = y.CategoryName,
+                                               Value = y.CategoryId.ToString(),
+                                           }).ToList();
+            ViewBag.v1 = value;
+
+            Food food = new Food()
+            {
+                FoodId = values.FoodId,
+                CategoryId = values.CategoryId,
+                FoodName = values.FoodName, 
+                Price = values.Price,   
+                Stock = values.Stock,
+                Description = values.Description,
+                ImageURL    = values.ImageURL
+            };
+
+            return View(food);
+        }
+        [HttpPost]
+        public IActionResult FoodUpdate(Food p)
+        {
+            var values = foodRepository.TGetList(p.FoodId);
+            values.FoodName = p.FoodName;
+            values.Stock    = p.Stock;
+            values.Price    = p.Price;
+            values.ImageURL = p.ImageURL;
+            values.Description = p.Description;
+            values.CategoryId = p.CategoryId;
+
+            foodRepository.TUpdate(values);
+            return RedirectToAction("Index");  
         }
     }
 }
