11fc52519dbfcd1ac0b92c3498f50308d826d9bf
Olya <119108499+Olyala94@users.noreply.github.com>
2023-08-02 20:19:33 +0300

    # - Kategorilerin Vitrinde Listelenmesi

---
+++ CoreAndFood/ViewComponents/CategoryGetList.cs
@@ -0,0 +1,15 @@
+﻿using CoreAndFood.Repository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreAndFood.ViewComponents
+{
+	public class CategoryGetList : ViewComponent
+	{
+		public IViewComponentResult Invoke()
+		{
+			CategoryRepository categoryRepository = new CategoryRepository();
+			var categorylist = categoryRepository.TList();
+			return View(categorylist);	
+		}
+	}
+}
