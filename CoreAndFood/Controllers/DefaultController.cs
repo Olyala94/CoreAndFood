f9dc51597aaff88c55c9dc50e9a1274aefde78bc
Olya <119108499+Olyala94@users.noreply.github.com>
2023-08-03 07:02:45 +0300

    # - Ürüün Listeleme işlemi yapıldı

--- CoreAndFood/Controllers/DefaultController.cs
+++ CoreAndFood/Controllers/DefaultController.cs
@@ -10,5 +10,10 @@ namespace CoreAndFood.Controllers
         {
             return View();
         }
+
+        public IActionResult CategoryDetails()
+        {
+            return View();  
+        }
     }
 }
